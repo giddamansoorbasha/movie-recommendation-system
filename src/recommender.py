c7169a2131ef9ef06826e3473ef36db3a91f40e5
G Mansoor <giddamansoor0@gmail.com>
2026-01-06 17:59:43 +0530

    Fix artifact loading for deploy

--- src/recommender.py
+++ src/recommender.py
@@ -1,15 +1,34 @@
+import os
 import pickle
 import pandas as pd
-import os
+from src.vectorization import vectorize
+from src.similarity import similarity as build_similarity
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ARTIFACTS_DIR = os.path.join(BASE_DIR, "artifacts")
+SIM_PATH = os.path.join(ARTIFACTS_DIR, "similarity.pkl")
+
 movies = pd.read_csv(os.path.join(BASE_DIR, "data/movies.csv"))
-similarity = pickle.load(open(os.path.join(BASE_DIR, "artifacts/similarity.pkl"), "rb"))
+
+if not os.path.exists(SIM_PATH):
+    os.makedirs(ARTIFACTS_DIR, exist_ok=True)
+    vectorize()        
+    build_similarity() 
+    
+with open(SIM_PATH, "rb") as f:
+    similarity = pickle.load(f)
 
 def recommend(movie, top_k=5):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), key=lambda x:x[1], reverse=True)[1:top_k+1]
-    recommended_movies = [(movies.iloc[i[0]]['title'],movies.iloc[i[0]]['poster_path']) for i in movies_list]
-    return recommended_movies
+    movies_list = sorted(
+        list(enumerate(distances)),
+        key=lambda x: x[1],
+        reverse=True
+    )[1:top_k+1]
+
+    return [
+        (movies.iloc[i[0]]['title'], movies.iloc[i[0]]['poster_path'])
+        for i in movies_list
+    ]
